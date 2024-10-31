a54e5baf743a2ab89ba9f6b6508a4c0c6a5c1e95
Zulqarnain Rajput <rajputzulqarnain055@gmail.com>
2024-10-31 17:14:44 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function clearform(){
+    document.getElementById("inputname").value="";
+    document.getElementById("inputmail").value="";
+    document.getElementById("inputfeedback").value="";
+}
